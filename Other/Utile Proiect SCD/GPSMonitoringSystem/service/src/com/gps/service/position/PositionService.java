7649368410bac4e55b8aa8b6b2d28334deaa785a
KovaxG <georgesmth202@gmail.com>
2016-11-30 18:02:08 +0200

    First Commit

--- Other/Utile Proiect SCD/GPSMonitoringSystem/service/src/com/gps/service/position/PositionService.java
+++
@@ -1,9 +0,0 @@
-package com.gps.service.position;
-
-/**
- * @author radu.miron
- * @since 10/8/13
- */
-public interface PositionService {
-    void savePosition(String jsonPosition) throws Exception;
-}
